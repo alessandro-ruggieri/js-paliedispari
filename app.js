ba2bc7feea05632a6b438a3995050cb20de67d9f
alessandro-ruggieri <alessandro.ruggieri95.ar@gmail.com>
2024-08-11 05:43:13 +0200

    COMPLETED: exercise "Pari o Dispari" in app.js

--- app.js
+++ app.js
@@ -35,3 +35,42 @@ function palindrome (parola) {
 
 let parolaUtente = prompt('Inserisci una parola e controlliamo se è palindroma!')
 palindrome (parolaUtente) 
+
+// PARI O DISPARI
+
+let numeroUtente = parseInt(prompt('Inserisci un numero compreso tra 1 e 5'))
+console.log('Il numero da te scelto è: ' + numeroUtente)
+let evenOrOdd = prompt('Inserisci "pari" se pensi che la somma con il numero del PC sarà pari, altrimenti "dispari"')
+console.log('Hai scelto: ' + evenOrOdd)
+let randomNumber
+
+function generateRandomNumber () {
+   randomNumber = Math.floor(Math.random()*5)+1
+   return randomNumber
+}
+
+function isEvenOrOdd (number) {
+   if (number % 2 === 0) {
+      numberType = 'pari'
+   } else {
+      numberType = 'dispari'
+   }
+   return numberType
+}
+
+generateRandomNumber ()
+console.log('Il numero scelto dal PC è: ' + randomNumber)
+
+sum = numeroUtente + randomNumber
+console.log('La somma del tuo numero e di quello del PC è: ' + sum)
+
+isEvenOrOdd (sum)
+console.log('Questa somma è: ' + numberType)
+
+if (numberType === evenOrOdd) {
+   console.log('Complimenti, hai vinto!')
+} else {
+   console.log('Mi dispiace, hai perso')
+}
+
+
